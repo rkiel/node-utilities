af535bb6d7ec7de0bdeca3b53fca11889932a38e
rkiel <cygnus@pobox.com>
2017-07-16 22:23:37 -0400

    init: finished init

--- lib/scripts/init.rb
+++ lib/scripts/init.rb
@@ -1,3 +1,4 @@
+require 'json'
 require_relative './base'
 
 module Scripts
@@ -12,7 +13,27 @@ module Scripts
     end
 
     def execute
-      #git_show_branches
+      if File.exist? 'package.json'
+        json = JSON.parse(File.read('package.json'))
+        puts 'reading package.json'
+      else
+        json = {
+          name: File.basename(Dir.pwd),
+          version: "1.0.0",
+          main: "index.js",
+        #  repository: "git@github.com:rkiel/node-utilities.git",
+        #  author: "rkiel <cygnus@pobox.com>",
+          license: "MIT"
+        }
+        File.write('package.json', JSON.pretty_generate(json))
+        puts 'creating package.json'
+      end
+
+      if not json["scripts"]
+        json["scripts"] = {}
+        File.write('package.json', JSON.pretty_generate(json))
+        puts 'updating package.json'
+      end
     end
   end
 
